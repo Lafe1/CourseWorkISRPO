8df56a49549795d79105716e93600230ca0694a6
Lafe1 <sharonov353@gmail.com>
2019-09-30 22:47:28 +0300

    Добавил начальную функцию для первого задания

--- CourseWork/Form1.cs
+++ CourseWork/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace CourseWork
 {
@@ -15,6 +16,36 @@ namespace CourseWork
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             //создаем элемент Chart
+        Chart myChart = new Chart();
+        //кладем его на форму и растягиваем на все окно.
+        myChart.Parent = this;
+        myChart.Dock = DockStyle.Fill;
+        //добавляем в Chart область для рисования графиков, их может быть
+        //много, поэтому даем ей имя.
+        myChart.ChartAreas.Add(new ChartArea("Math functions"));
+        //Создаем и настраиваем набор точек для рисования графика, в том
+        //не забыв указать имя области на которой хотим отобразить этот
+        //набор точек.
+        Series mySeriesOfPoint = new Series("Sinus");
+        mySeriesOfPoint.ChartType = SeriesChartType.Line;
+        mySeriesOfPoint.ChartArea = "Math functions";
+        for (double x = -10; x <= 10; x += 1) 
+        {
+            mySeriesOfPoint.Points.AddXY(x, function(x));
+        }
+        //Добавляем созданный набор точек в Chart
+        myChart.Series.Add(mySeriesOfPoint);
+        }
+
+        public double function(double x)
+        {
+            double y = Math.Round(Math.Pow(Math.E, (-x)));
+            return y;
         }
     }
 }
