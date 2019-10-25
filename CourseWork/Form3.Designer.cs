b2bb8eae457f38e6ccac653615eb1783b5d8e674
Lafe1 <sharonov353@gmail.com>
2019-10-25 15:41:30 +0300

    Доработал 2 задание

--- CourseWork/Form3.Designer.cs
+++ CourseWork/Form3.Designer.cs
@@ -31,12 +31,14 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.вывестиСписокToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.вывестиСписокToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -50,7 +52,7 @@
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 3, 0, 3);
-            this.menuStrip1.Size = new System.Drawing.Size(840, 32);
+            this.menuStrip1.Size = new System.Drawing.Size(844, 32);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -69,6 +71,14 @@
             this.открытьToolStripMenuItem.Size = new System.Drawing.Size(160, 26);
             this.открытьToolStripMenuItem.Text = "Открыть";
             // 
+            // вывестиСписокToolStripMenuItem
+            // 
+            this.вывестиСписокToolStripMenuItem.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.вывестиСписокToolStripMenuItem.Name = "вывестиСписокToolStripMenuItem";
+            this.вывестиСписокToolStripMenuItem.Size = new System.Drawing.Size(160, 26);
+            this.вывестиСписокToolStripMenuItem.Text = "Вывести список";
+            this.вывестиСписокToolStripMenuItem.Click += new System.EventHandler(this.вывестиСписокToolStripMenuItem_Click);
+            // 
             // dataGridView1
             // 
             this.dataGridView1.AllowUserToAddRows = false;
@@ -84,24 +94,24 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 568);
+            this.label1.Location = new System.Drawing.Point(44, 568);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(205, 22);
+            this.label1.Size = new System.Drawing.Size(229, 22);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Введите номер класса:";
+            this.label1.Text = "Введите название класса:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(223, 565);
+            this.textBox1.Location = new System.Drawing.Point(279, 565);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(112, 30);
             this.textBox1.TabIndex = 3;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(341, 554);
+            this.button1.Location = new System.Drawing.Point(452, 553);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(207, 53);
+            this.button1.Size = new System.Drawing.Size(258, 53);
             this.button1.TabIndex = 4;
             this.button1.Text = "Вывести список учеников";
             this.button1.UseVisualStyleBackColor = true;
@@ -109,7 +119,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(570, 553);
+            this.button2.Location = new System.Drawing.Point(452, 625);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(258, 53);
             this.button2.TabIndex = 5;
@@ -117,19 +127,29 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // вывестиСписокToolStripMenuItem
+            // textBox2
             // 
-            this.вывестиСписокToolStripMenuItem.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.вывестиСписокToolStripMenuItem.Name = "вывестиСписокToolStripMenuItem";
-            this.вывестиСписокToolStripMenuItem.Size = new System.Drawing.Size(160, 26);
-            this.вывестиСписокToolStripMenuItem.Text = "Вывести список";
-            this.вывестиСписокToolStripMenuItem.Click += new System.EventHandler(this.вывестиСписокToolStripMenuItem_Click);
+            this.textBox2.Location = new System.Drawing.Point(279, 637);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(112, 30);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 640);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(205, 22);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Введите номер класса:";
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(840, 619);
+            this.ClientSize = new System.Drawing.Size(844, 690);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
@@ -161,5 +181,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ToolStripMenuItem вывестиСписокToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
     }
 }
--- CourseWork/Form3.cs
+++ CourseWork/Form3.cs
@@ -71,6 +71,7 @@ namespace CourseWork
             
 
             List<string[]> Student = new List<string[]>();
+            
             for (int i = 0; i < dataGridView1.Rows.Count; i++)
             {
                 if (dataGridView1.Rows[i].Cells[1].Value.ToString() == textBox1.Text)
@@ -97,7 +98,55 @@ namespace CourseWork
 
         private void button2_Click(object sender, EventArgs e)
         {
-            
+            dataGridView1.Rows.Clear();
+            foreach (string[] s in Memory)
+                dataGridView1.Rows.Add(s);
+
+            char ch;
+            int n = 0;
+            char[] numClass = new char[32];
+            int[] arrCount = new int[32];
+            for (int i = 1040; i <= 1071; i++)
+            {
+                ch = System.Convert.ToChar(i);
+                numClass[n] = ch;
+                n++;
+            }
+
+            List<string[]> Student = new List<string[]>();
+            int count = 0;
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                if (dataGridView1.Rows[i].Cells[1].Value.ToString().Substring(0, 2) == textBox2.Text.ToString().Substring(0, 2))
+                {
+                    count++;
+                    //Student.Add(new string[2]);
+                    //Student[Student.Count - 1][0] = dataGridView1.Rows[i].Cells[0].Value.ToString();
+                    //Student[Student.Count - 1][1] = ;
+                }
+            }
+            for (int i = 0; i < 32; i++)
+            {
+                for (int j = 0; j < dataGridView1.Rows.Count; j++)
+                {
+                    if (dataGridView1.Rows[j].Cells[1].Value.ToString() == textBox2.Text.ToString() + numClass[i])
+                    {
+
+                        arrCount[i]++;
+                        //Student.Add(new string[2]);
+                        //Student[Student.Count - 1][0] = dataGridView1.Rows[i].Cells[0].Value.ToString();
+                        //Student[Student.Count - 1][1] = ;
+                    }
+                }
+                if (arrCount[i] > 0)
+                {
+                    Student.Add(new string[2]);
+                    Student[Student.Count - 1][0] = textBox2.Text.ToString() + numClass[i];
+                    Student[Student.Count - 1][1] = arrCount[i].ToString();
+                }
+                //dataGridView1.Rows.Add(textBox1.Text.ToString() + numClass[i], arrCount[i]);
+            }
+
             dataGridView1.Rows.Clear();
 
             dataGridView1.ColumnCount = 2;
@@ -109,7 +158,15 @@ namespace CourseWork
 
             dataGridView1.Columns[0].HeaderText = "Название класса";
             dataGridView1.Columns[1].HeaderText = "Кол-во учеников";
-            
+
+            dataGridView1.Rows.Add(textBox2.Text, count.ToString());
+            foreach (string[] s in Student)
+                dataGridView1.Rows.Add(s);
+
+
+            //foreach (string[] s in Student)
+            // dataGridView1.Rows.Add(s);
+
         }
 
         private void вывестиСписокToolStripMenuItem_Click(object sender, EventArgs e)
@@ -137,13 +194,13 @@ namespace CourseWork
                 dataGridView1.Rows.Add(row);
             }
 
-            for (int i = 0; i < dataGridView1.Rows.Count; i++)
-            {
+            //for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            //{
 
-                Memory.Add(new string[2]);
-                Memory[Memory.Count - 1][0] = dataGridView1.Rows[i].Cells[0].Value.ToString();
-                Memory[Memory.Count - 1][1] = dataGridView1.Rows[i].Cells[1].Value.ToString();
-            }
+            //    Memory.Add(new string[2]);
+            //    Memory[Memory.Count - 1][0] = dataGridView1.Rows[i].Cells[0].Value.ToString();
+            //    Memory[Memory.Count - 1][1] = dataGridView1.Rows[i].Cells[1].Value.ToString();
+            //}
         }
     }
 }
